78e42dd6bce1f7524ccf6e25fe4a75c7bb07ce25
Bhaskar001 <bhaskarjoshi900@gmail.com>
2024-09-10 01:25:01 +0530

    1

--- Javascript.js/index.js
+++ Javascript.js/index.js
@@ -1,8 +1,24 @@
-function waitFor3Sec(resolve){
+function waitFor3Sec(resolve){ //resolve = main
     setTimeout(resolve, 3000)
+}
+
+function setTimeoutPromisified(){
+    return new Promise(waitFor3Sec)
 }
 
 function main(){
     console.log("main is calles")
 }
-waitFor3Sec(main)
+
+setTimeoutPromisified().then(main);
+
+// function setTimeoutPromisified(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   }
+  
+//   function callback() {
+//       console.log("3 seconds have passed");
+//   }
+  
+//   setTimeoutPromisified(3000).then(callback)
+  
